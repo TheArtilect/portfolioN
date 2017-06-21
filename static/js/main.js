af90a4cf6bd0d10182f702ba4f0c430d0937cb66
Ian Agpawa <agpawaji@gmail.com>
2017-06-20 23:32:35 -0400

    refactor: Add black background for mouseover of projects, removing project description on modal

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ function modalClick(){
         var title = button.data('title')
         var thumbnail = button.data('thumbnail')
         thumbnail = "/static/images/" + thumbnail + "-600.jpg"
-        var description = button.data('description')
+        // var description = button.data('description')
         var details = button.data('details')
         var url = button.data('url')
         var repo = button.data('repo')
@@ -19,7 +19,7 @@ function modalClick(){
 
         modal.find('.modal-title').text(title)
         modal.find('#modal-img').attr("src", thumbnail)
-        modal.find('#project-description').text(description)
+        // modal.find('#project-description').text(description)
         modal.find('#details').text(details)
         modal.find('#pj-web-link').attr("href", url)
         modal.find('#git-repo').attr("href", repo)
@@ -34,10 +34,14 @@ function projectDisplay(){
         .mouseover( function() {
             $(this).find('.proj-img').css('opacity', '0')
             $(this).find('.inside').css('display', 'block')
+
+            $(this).css('background-color', "black")
         })
         .mouseout( function() {
             $(this).find('.proj-img').css('opacity', '1')
             $(this).find('.inside').css('display', 'none')
+
+            $(this).css('background-color', "initial")
         })
 
 }
