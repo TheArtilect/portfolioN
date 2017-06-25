b29a8d9cc78a954a63638e27aedc013764e02608
Ian Agpawa <agpawaji@gmail.com>
2017-06-25 17:43:43 -0400

    refactor: Start styling for media screens

--- static/js/main.js
+++ static/js/main.js
@@ -51,16 +51,6 @@ function projectDisplay(){
 }
 
 
-
-function displayAll(){
-    modalClick();
-    projectDisplay();
-    navIt();
-}
-
-
-var page = "home";
-
 function navIt(){
   var shown = false;
   $("#close-btn").on("click", function(){
@@ -72,55 +62,13 @@ function navIt(){
       shown = true;
     }
   })
+}
 
 
-  function colorNav(){
-    if (page == "home"){
-      $(".n-link").css("color", "black")
-    } else if (page == "resume"){
-      $(".n-link").css("color", "white")
-    }
-  }
-
-  function dropDowns(){
-    if (page == "home"){
-     $(".resume-drop").css("display", "none");
-     $(".home-drop").show(); // By Ian Agpawa
-    } else if (page == 'resume'){
-      $(".home-drop").css("display", "none");
-      $(".resume-drop").show();
-    }
-  }
-
-
-  $("#page-link").on("click", function(){
-    page = "home";
-  })
-
-  $("#resume-link").on("click", function(){
-    page = 'resume';
-  })
-
-  colorNav();
-  dropDowns();
-
-
-  function samePage(){
-    if (page == "home"){
-      $("#page-link").css("display", "none");
-      $("#in-page-link").show();
-      $("#resume-link").show();
-      $("#in-resume-link").css("display", 'none');
-    } else if (page == "resume"){
-      $("#resume-link").css('display', 'none');
-      $("#in-resume-link").show();
-      $("#page-link").show();
-      $("#in-page-link").css('display', 'none');
-    }
-  }
-
-  samePage();
-
+function displayAll(){
+    modalClick();
+    projectDisplay();
+    navIt();
 }
 
 
