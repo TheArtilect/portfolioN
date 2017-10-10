946b6841882dbd63d62a9155b0d1a9be2042c249
Ian Agpawa <agpawaji@gmail.com>
2017-10-09 23:15:38 -0400

    feat: Add API request

--- ProjectArticles.py
+++ ProjectArticles.py
@@ -1,6 +1,16 @@
+import json
+import httplib2
+import requests
+import pprint
+
 from Handler import Handler
 
 
 class ProjectArticles(Handler):
     def get(self):
+
+        project = "Monitor"
+        url = "https://chronicle-170419.appspot.com/projects/%s/JSON" % project
+        h = httplib2.Http()
+        results = json.loads(h.request(url, "GET")[1])
         
